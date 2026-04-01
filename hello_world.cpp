3230ca9c8de7c7a993816170598347834fcc6c3a
PavelDats1 <tiger206135@gmail.com>
2026-04-01 16:56:52 +0300

    Add comments to code

--- hello_world.cpp
+++ hello_world.cpp
@@ -2,9 +2,13 @@
 #include <string>
 int main()
 {
+    // Variable to store user's name
     std::string name;
+    // Prompt user for input
     std::cout << "Enter your name: ";
     std::cin >> name;
+
+    // Display greeting
     std::cout << "Hello world from " << name << std::endl;
     return 0;
 }
